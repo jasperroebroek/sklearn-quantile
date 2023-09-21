8be92e343bf2bcf189a0d59f711fe40617455a40
jroebroek <roebroek.jasper@gmail.com>
2023-09-21 13:22:05 +0200

    pyproject.toml

--- setup.py
+++ setup.py
@@ -20,21 +20,7 @@ extensions = [
 ]
 
 setup(
-    name='sklearn_quantile',
-    version='0.0.23',
     packages=find_packages(),
-    url='https://github.com/jasperroebroek/sklearn-quantile',
-    license='BSD 3 clause',
-    author='Jasper Roebroek',
-    author_email='roebroek.jasper@gmail.com',
     ext_modules=cythonize(extensions, annotate=True),
-    include_dirs=[numpy.get_include()],
-    setup_requires=['cython', 'numpy', 'setuptools', 'packaging'],
-    install_requires=['scikit-learn>=1.0', 'cython'],
-    extras_require={
-        'develop': ['cython', 'scikit-learn>=1.0', 'sphinx', 'sphinx_rtd_theme', 'numpydoc', 'jupyter', 'matplotlib',
-                    'pandas', 'packaging']
-    },
-    long_description=read_file('README.md'),
-    long_description_content_type='text/markdown',
+    include_dirs=[numpy.get_include()]
 )
