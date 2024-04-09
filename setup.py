c2f71c9f3b3294e0b0f3df165cf047f6359b5a17
jroebroek <roebroek.jasper@gmail.com>
2024-04-09 12:03:41 +0200

    Pytest in github actions

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@ from setuptools import setup, find_packages
 from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
-import os
-
-
-def read_file(filename):
-    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
-        return file.read()
 
 
 extensions = [
@@ -20,7 +14,6 @@ extensions = [
 ]
 
 setup(
-    name="sklearn_quantile",
     packages=find_packages(),
     ext_modules=cythonize(extensions, annotate=True, language_level="3"),
     include_dirs=[numpy.get_include()]
