Fix bugs; (1) RandomForestMaximumRegressor only generating positive outputs, (2) weights not being taken into account in the KNN regressor, (3) weighted quantile calculations silently not accepting weights in certain conditions

sklearn_quantile/neighbors/quantile.py
@@ -130,18 +130,19 @@ class KNeighborsQuantileRegressor(KNeighborsRegressor, QuantileRegressorMixin):
     https://en.wikipedia.org/wiki/K-nearest_neighbors_algorithm
 
     """
+
     def __init__(
-            self,
-            n_neighbors=5,
-            q=None,
-            *,
-            weights="uniform",
-            algorithm="auto",
-            leaf_size=30,
-            p=2,
-            metric="minkowski",
-            metric_params=None,
-            n_jobs=None,
+        self,
+        n_neighbors=5,
+        q=None,
+        *,
+        weights="uniform",
+        algorithm="auto",
+        leaf_size=30,
+        p=2,
+        metric="minkowski",
+        metric_params=None,
+        n_jobs=None,
     ):
         super().__init__(
             n_neighbors=n_neighbors,
@@ -151,7 +152,7 @@ class KNeighborsQuantileRegressor(KNeighborsRegressor, QuantileRegressorMixin):
             p=p,
             metric_params=metric_params,
             n_jobs=n_jobs,
-            weights=weights
+            weights=weights,
         )
         self.q = q
 
@@ -167,13 +168,13 @@ class KNeighborsQuantileRegressor(KNeighborsRegressor, QuantileRegressorMixin):
 
         Returns
         -------
-        y : ndarray of shape (n_queries,), (n_quantiles, n_queries),  (n_queries, n_outputs) or
+        y : ndarray of shape (n_queries), (n_quantiles, n_queries), (n_queries, n_outputs) or
             (n_quantiles, n_queries, n_outputs), dtype=float
             Target values. Shape depends on the number of requested quantiles and outputs, with
             the simplest case returning a 1d array corresponding to the number of queries.
         """
         q = self.validate_quantiles()
-        X = self._validate_data(X, accept_sparse='csr', reset=False)
+        X = self._validate_data(X, accept_sparse="csr", reset=False)
 
         neigh_dist, neigh_ind = self.kneighbors(X)
 
@@ -188,12 +189,11 @@ class KNeighborsQuantileRegressor(KNeighborsRegressor, QuantileRegressorMixin):
             weights = np.broadcast_to(weights[:, :, np.newaxis], a.shape)
 
         # this falls back on np.quantile if weights is None
-        y_pred = weighted_quantile(a, q, weights, axis=1)
+        y_pred = weighted_quantile(
+            a=a, q=q, weights=weights, axis=1, interpolation="linear"
+        )
 
         if self._y.ndim == 1:
             y_pred = y_pred[..., 0]
-
-        if q.size == 1:
-            y_pred = y_pred[0]
 
         return y_pred

tests/test_weighted_quantile.py
@@ -1,9 +1,14 @@
+from functools import partial
+
 import numpy as np
 import pytest
-from numpy.testing import assert_array_almost_equal, assert_equal, assert_almost_equal, assert_raises
+from numpy.testing import assert_almost_equal, assert_array_almost_equal, assert_equal
+from sklearn_quantile.utils.weighted_quantile import (
+    WeightedQuantileCalculator,
+    weighted_quantile as py_weighted_quantile,
+)
 
-from sklearn_quantile.utils.weighted_quantile import WeightedQuantileCalculator
-from sklearn_quantile.utils.weighted_quantile import weighted_quantile as py_weighted_quantile
+np_quantile = partial(np.quantile, method="interpolated_inverted_cdf")
 
 
 @pytest.fixture
@@ -20,7 +25,7 @@ def test_quantile_equal_weights_overlapping(weighted_quantile):
     q = np.arange(0.1, 1.0, 0.1)
 
     # since weights are equal, quantiles overlap with the values
-    actual = weighted_quantile(x, weights, q)
+    actual = weighted_quantile(a=x, weights=weights, q=q)
     assert_array_almost_equal(actual, np.sort(x)[:-1])
 
 
@@ -31,7 +36,7 @@ def test_quantile_equal_weights_not_overlapping(weighted_quantile):
     weights = 0.1 * np.ones(10)
     q = np.arange(0.05, 1.05, 0.1)
 
-    actual = weighted_quantile(x, weights, q)
+    actual = weighted_quantile(a=x, weights=weights, q=q)
 
     sorted_x = np.sort(x)
     expected = np.hstack(((sorted_x[0],), 0.5 * (sorted_x[1:] + sorted_x[:-1])))
@@ -39,27 +44,47 @@ def test_quantile_equal_weights_not_overlapping(weighted_quantile):
     assert_array_almost_equal(actual, expected)
 
 
-def test_quantile_toy_data(weighted_quantile):
+def test_weighted_quantile_toy_data(weighted_quantile):
     x = [1, 2, 3]
     weights = [1, 4, 5]
 
-    assert_equal(weighted_quantile(x, weights, 0.0), 1)
-    assert_equal(weighted_quantile(x, weights, 1.0), 3)
+    assert_equal(weighted_quantile(a=x, weights=weights, q=0.0), 1)
+    assert_equal(weighted_quantile(a=x, weights=weights, q=1.0), 3)
 
-    assert_equal(weighted_quantile(x, weights, 0.05), 1)
-    assert_almost_equal(weighted_quantile(x, weights, 0.30), 1.5)
-    assert_equal(weighted_quantile(x, weights, 0.75), 2.5)
-    assert_almost_equal(weighted_quantile(x, weights, 0.50), 2)
+    assert_equal(weighted_quantile(a=x, weights=weights, q=0.05), 1)
+    assert_almost_equal(weighted_quantile(a=x, weights=weights, q=0.30), 1.5)
+    assert_equal(weighted_quantile(a=x, weights=weights, q=0.75), 2.5)
+    assert_almost_equal(weighted_quantile(a=x, weights=weights, q=0.50), 2)
 
 
-@pytest.mark.parametrize('q', [0, 0.1, 0.5, 0.9, 1])
+@pytest.mark.parametrize("q", [0, 0.1, 0.5, 0.9, 1])
+def test_quantile_toy_data(q, weighted_quantile):
+    x = [1, 2, 3]
+    assert_almost_equal(
+        weighted_quantile(a=x, weights=None, q=q), np_quantile(a=x, q=q), decimal=3
+    )
+
+
+def test_one_sample_per_quantile(weighted_quantile):
+    x = np.asarray([1])
+    weights = None
+
+    assert_equal(weighted_quantile(a=x, weights=weights, q=0), 1)
+    assert_equal(py_weighted_quantile(x, weights=weights, q=0), 1)
+
+    weights = np.asarray([1])
+    assert_equal(weighted_quantile(a=x, weights=weights, q=0), 1)
+    assert_equal(py_weighted_quantile(x, weights=weights, q=0), 1)
+
+
+@pytest.mark.parametrize("q", [0, 0.1, 0.5, 0.9, 1])
 def test_zero_weights(q, weighted_quantile):
     x = [1, 2, 3, 4, 5]
     w = [0, 0, 0, 0.1, 0.1]
 
     assert_equal(
-        weighted_quantile(x, w, q),
-        weighted_quantile([4, 5], [0.1, 0.1], q)
+        weighted_quantile(a=x, weights=w, q=q),
+        weighted_quantile(a=[4, 5], weights=[0.1, 0.1], q=q),
     )
 
 
@@ -69,12 +94,14 @@ def test_return_shapes_no_weights(keepdims):
     x = rng.randn(100, 10, 20)
 
     assert (
-        py_weighted_quantile(x, 0.5, weights=None, axis=1, keepdims=keepdims).shape ==
-        np.quantile(x, 0.5, axis=1, keepdims=keepdims).shape
+        py_weighted_quantile(x, 0.5, weights=None, axis=1, keepdims=keepdims).shape
+        == np.quantile(x, 0.5, axis=1, keepdims=keepdims).shape
     )
     assert (
-        py_weighted_quantile(x, (0.5, 0.5), weights=None, axis=1, keepdims=keepdims).shape ==
-        np.quantile(x, (0.5, 0.5), axis=1, keepdims=keepdims).shape
+        py_weighted_quantile(
+            x, (0.5, 0.5), weights=None, axis=1, keepdims=keepdims
+        ).shape
+        == np.quantile(x, (0.5, 0.5), axis=1, keepdims=keepdims).shape
     )
 
 
@@ -87,12 +114,12 @@ def test_return_shapes_with_axis(keepdims, axis):
 
     # shape should be the same as the output of np.quantile. Without weights it is actually the same calculation
     assert (
-        py_weighted_quantile(x, 0.5, weights, axis=axis, keepdims=keepdims).shape ==
-        np.quantile(x, 0.5, axis=axis, keepdims=keepdims).shape
+        py_weighted_quantile(x, 0.5, weights, axis=axis, keepdims=keepdims).shape
+        == np.quantile(x, 0.5, axis=axis, keepdims=keepdims).shape
     )
     assert (
-        py_weighted_quantile(x, (0.5, 0.8), weights, axis=axis, keepdims=keepdims).shape ==
-        np.quantile(x, (0.5, 0.8), axis=axis, keepdims=keepdims).shape
+        py_weighted_quantile(x, (0.5, 0.8), weights, axis=axis, keepdims=keepdims).shape
+        == np.quantile(x, (0.5, 0.8), axis=axis, keepdims=keepdims).shape
     )
 
 
@@ -103,12 +130,15 @@ def test_return_shapes_without_axis(keepdims):
     weights = 0.01 * np.ones_like(x)
 
     assert (
-        py_weighted_quantile(x, 0.5, weights, axis=None, keepdims=keepdims).shape ==
-        np.quantile(x, 0.5, axis=None, keepdims=keepdims).shape
+        py_weighted_quantile(x, 0.5, weights, axis=None, keepdims=keepdims).shape
+        == np.quantile(x, 0.5, axis=None, keepdims=keepdims).shape
     )
 
     if not keepdims:
-        assert isinstance(py_weighted_quantile(x, 0.5, weights, axis=None, keepdims=keepdims), (np.float32, float))
+        assert isinstance(
+            py_weighted_quantile(x, 0.5, weights, axis=None, keepdims=keepdims),
+            (np.float32, float),
+        )
 
 
 def test_errors():
@@ -117,4 +147,5 @@ def test_errors():
     weights = 0.01 * np.ones_like(x)
 
     # axis should be integer
-    assert_raises(NotImplementedError, py_weighted_quantile, x, 0.5, weights, axis=(1, 2))
+    with pytest.raises(NotImplementedError):
+        py_weighted_quantile(x, 0.5, weights, axis=(1, 2))